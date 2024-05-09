0f36e1dc77e83d13d2a5842ee50fd938069441f5
Mauro F. Giraudo <mfgiraudo456@gmail.com>
2024-05-09 18:38:02 -0300

    visto hasta " video API - Unhandled Errors"

--- api_practice/src/app.ts
+++ api_practice/src/app.ts
@@ -1,8 +1,9 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { Character } from './character.js'
 
 const port = 3000
 const app = express()
+app.use(express.json())
 
 //app.use('/', (req, res) => {
 //  res.json({"message": "<h1>Hello!!</h1>"})
@@ -16,32 +17,111 @@ const characters = [
     2800,
     130,
     3030,
-    ['Ice Sword', 'Blindfold']
-  )
+    ['Ice Sword', 'Blindfold'],
+    "b780c4c6-68c2-4177-8371-af0102a99d38"
+  ),
+  new Character(
+    'Acheron',
+    'Nihility',
+    80,
+    3157,
+    9,
+    3814,
+    ['Electric Sword', 'Crimson Blossom'],
+    "8f4473bf-6871-42c1-bbd6-f5cfaa859de1")
 ]
 
-//Función "Get All"
+function sanitizeCharacterInput(req: Request, res: Response, next: NextFunction) {
+  req.body.sanitizedCharacter = {
+    name: req.body.name,
+    characterClass: req.body.characterClass,
+    level: req.body.level,
+    hp: req.body.hp,
+    mana: req.body.mana,
+    attack: req.body.attack,
+    items: req.body.items
+  }
+  Object.keys(req.body.sanitizedCharacter).forEach((key) => {
+    if (req.body.sanitizedCharacter[key] === undefined) {
+      delete req.body.sanitizedCharacter[key]
+    }
+  })
+  next()
+}
+
+//get /api/characters/ -> obtener lista de characters
 app.get('/api/characters', (req, res) => {
-  res.json(characters)
+  return res.json({data: characters})
 })
 
+//get /api/characters/:id -> obtener el character con id = :id
 app.get('/api/characters/:id', (req, res) => {
   const character = characters.find((character) => character.id === req.params.id)
   if (!character) {
-    res.status(404).send({message: 'characterNotFound'})
+    return res.status(404).send({message: 'characterNotFound'})
+    //el "return" evita que continúe la ejecución hacia "res.json"
+  } 
+  res.json({data: character})
+})
+
+//post /api/character -> crear nuevos character
+
+//user -> request -> express -> middleware que forme req.body (express.json()) -> app.post -> response -> user
+
+app.post('/api/characters', sanitizeCharacterInput, (req, res) => {
+  const input = req.body.sanitizedCharacter
+
+  const character = new Character(input.name, input.characterClass, input.level, input.hp, input.mana, input.attack, input.items)
+
+  characters.push(character)
+
+  return res.status(201).send({message: 'Character created succesfully', data: character})
+})
+
+//put /api/characters/:id -> modificar character con id = :id (recurso completo e idempotente)
+app.put('/api/characters/:id', sanitizeCharacterInput, (req, res) => {
+  const charIndex = characters.findIndex((character) => character.id === req.params.id)
+  if (charIndex === -1) {
+    return res.status(404).send({message: 'characterNotFound'})
+  }
+
+  Object.assign(characters[charIndex], req.body.sanitizedCharacter)
+  //También podemos reasignar de la siguiente manera:
+  //characters[charIndex] = {...characters[charIndex], ...req.body.sanitizedCharacter}
+
+  return res.status(200).send({message: 'Character modified succesfully', data: characters[charIndex]})
+})
+
+//patch /api/characters/:id -> modificar character con id = :id (recurso parcial)
+app.patch('/api/characters/:id', sanitizeCharacterInput, (req, res) => {
+  const charIndex = characters.findIndex((character) => character.id === req.params.id)
+  if (charIndex === -1) {
+    return res.status(404).send({message: 'characterNotFound'})
+  }
+
+  characters[charIndex] = {...characters[charIndex], ...req.body.sanitizedCharacter}
+  return res.status(200).send({message: 'Character modified succesfully', data: characters[charIndex]})
+})
+
+//
+app.delete('/api/characters/:id', (req, res) => {
+  const charIndex = characters.findIndex((character) => character.id === req.params.id)
+  if (charIndex === -1) {
+    res.status(404).send({message: 'Character not found'})
   } else {
-    res.json(character)
+  characters.splice(charIndex, 1)
+  res.status(200).send({message: 'Character deleted succesfully'})
   }
 })
 
+app.use((req, res) => {
+  return res.status(404).send({message: 'Resource not found'})
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`)
 })
 
 //character -> /api/characters
-//get /api/characters/ -> obtener lista de characters
-//get /api/characters/:id -> obtener el character con id = :id
-//post /api/character -> crear nuevos character
 //detele /api/character/:id -> borrar character con id = :id
 //put & patch /api/characters/:id -> modificar character con id = :id
