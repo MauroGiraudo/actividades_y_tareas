eccd4c8b1dfc0f9dcf61c58e20e9bf11474fef5f
Mauro F. Giraudo <mfgiraudo456@gmail.com>
2024-08-07 01:20:25 -0300

    Tour of Heroes hasta paso 5 (HAY ERRORES EN ESTE ÚLTIMO PASO)

---
+++ actividades_angular/tour_of_heroes/tour_of_heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.js";
+
+export const HEROES: Hero[] = [
+  {id: 12, name: 'Gon Freecs'},
+  {id: 13, name: 'Killua Zoldyck'},
+  {id: 14, name: 'Leorio'},
+  {id: 15, name: 'Kurapika'},
+  {id: 16, name: 'Kite'},
+  {id: 17, name: 'Ging Freecs'},
+  {id: 18, name: 'Hisoka'},
+  {id: 19, name: 'Ikalgo'},
+  {id: 20, name: 'Meleoron'},
+  {id: 21, name: 'Netero'}
+]
